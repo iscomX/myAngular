5556bc422d6546576c51c68d1247a73bc1abd833
iscom git <iscom321@gmail.com>
2018-02-09 16:43:35 +0000

    01-add Components

---
+++ src/app/header/header.componet.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.componet.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+  constructor() {}
+
+
+}
