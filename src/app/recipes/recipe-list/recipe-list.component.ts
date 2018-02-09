3fc5bc768f77ca85644b4b5f381d3dcc52d24867
iscom git <iscom321@gmail.com>
2018-02-09 17:38:31 +0000

    02-add recipes list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe-model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe-1',
+      'This is simply a test-1',
+      'assets/img_1.png'
+    ),
+    new Recipe(
+      'A Test Recipe-2',
+      'This is simply a test-2',
+      'assets/img_2.png'
+    ),
+    new Recipe(
+      'A Test Recipe-3',
+      'This is simply a test-3',
+      'assets/img_3.png'
+    )
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe-model';
 
 @Component({
   selector: 'app-recipes',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('test recipe', 'this is a simple test', 'assets/001C.png')
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
